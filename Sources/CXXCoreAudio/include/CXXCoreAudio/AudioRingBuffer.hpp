d361e4f4f59d7860f686ac6f80c2e205673d93fc
Stephen F. Booth <me@sbooth.org>
2025-11-17 08:14:50 -0600

    Remove unnecessary `explicit`

--- Sources/CXXCoreAudio/include/CXXCoreAudio/AudioRingBuffer.hpp
+++ Sources/CXXCoreAudio/include/CXXCoreAudio/AudioRingBuffer.hpp
@@ -38,7 +38,7 @@ public:
 	/// @param format The format of the audio that will be written to and read from this buffer.
 	/// @param size The desired buffer capacity per channel, in audio frames.
 	/// @throw std::bad_alloc if memory could not be allocated or std::invalid_argument if the buffer size is not supported.
-	explicit AudioRingBuffer(const AudioStreamBasicDescription& format, uint32_t size);
+	AudioRingBuffer(const AudioStreamBasicDescription& format, uint32_t size);
 
 	// This class is non-copyable
 	AudioRingBuffer(const AudioRingBuffer&) = delete;
