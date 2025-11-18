a1c60a9207d0471816350db038d791fa34f753c4
Stephen Booth <me@sbooth.org>
2025-11-17 20:33:39 -0600

    Add basic tests for `CARingBuffer` (#23)

--- Tests/CXXCoreAudioTests/CXXCoreAudioTests.swift
+++ Tests/CXXCoreAudioTests/CXXCoreAudioTests.swift
@@ -68,6 +68,49 @@ import Testing
 		let std2ch = AudioStreamBasicDescription(mSampleRate: 44100, mFormatID: kAudioFormatLinearPCM, mFormatFlags: kAudioFormatFlagsNativeFloatPacked|kAudioFormatFlagIsNonInterleaved, mBytesPerPacket: 8, mFramesPerPacket: 8, mBytesPerFrame: 8, mChannelsPerFrame: 2, mBitsPerChannel: 32, mReserved: 0)
 		#expect(rb.Allocate(std2ch, 512) == true)
 		#expect(rb.Capacity() == 511)
-		#expect(empty.UnusedSpace() == empty.Capacity())
+		#expect(rb.UnusedSpace() == rb.Capacity())
+
+		let mono8bit = AudioStreamBasicDescription(mSampleRate: 22050, mFormatID: kAudioFormatLinearPCM, mFormatFlags: kAudioFormatFlagIsPacked|kAudioFormatFlagsNativeEndian|kAudioFormatFlagIsNonInterleaved, mBytesPerPacket: 1, mFramesPerPacket: 1, mBytesPerFrame: 1, mChannelsPerFrame: 1, mBitsPerChannel: 8, mReserved: 0)
+
+		let input_abl = AudioBufferList.allocate(maximumBuffers: 1)
+		input_abl[0] = AudioBuffer(mNumberChannels: 1, mDataByteSize: 128, mData: malloc(128))
+		defer {
+			for buffer in input_abl {
+				free(buffer.mData)
+			}
+			free(input_abl.unsafeMutablePointer)
+		}
+
+		let output_abl = AudioBufferList.allocate(maximumBuffers: 1)
+		output_abl[0] = AudioBuffer(mNumberChannels: 1, mDataByteSize: 128, mData: malloc(128))
+		defer {
+			for buffer in output_abl {
+				free(buffer.mData)
+			}
+			free(output_abl.unsafeMutablePointer)
+		}
+
+		memset(input_abl[0].mData, 0xA, 32)
+		memset(input_abl[0].mData?.advanced(by: 32), 0xB, 32)
+		memset(input_abl[0].mData?.advanced(by: 64), 0xC, 32)
+		memset(input_abl[0].mData?.advanced(by: 96), 0xD, 32)
+
+		memset(output_abl[0].mData, 0, 128)
+
+		#expect(rb.Allocate(mono8bit, 256) == true)
+
+		#expect(rb.Write(input_abl.unsafePointer, 128, 0) == true)
+		#expect(rb.GetTimeBounds(&start, &end) == true)
+		#expect(start == 0)
+		#expect(end == 128)
+
+		#expect(rb.Read(output_abl.unsafeMutablePointer, 128, 0) == true)
+		#expect(output_abl[0].mDataByteSize == 128)
+		#expect(memcmp(input_abl[0].mData, output_abl[0].mData, 128) == 0)
+
+		#expect(rb.Read(output_abl.unsafeMutablePointer, 128, 64) == true)
+		#expect(output_abl[0].mDataByteSize == 64)
+
+		#expect(memcmp(input_abl[0].mData?.advanced(by: 64), output_abl[0].mData, 64) == 0)
 	}
 }
