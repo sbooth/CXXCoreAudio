5a16848242ff79eb6b88ac05c9eae0e6109de524
Stephen F. Booth <me@sbooth.org>
2025-11-24 21:52:04 -0600

    Assume mono or stereo for 1 or 2 channels

--- Sources/CXXCoreAudio/CAAudioFormat.cpp
+++ Sources/CXXCoreAudio/CAAudioFormat.cpp
@@ -13,6 +13,11 @@ CXXCoreAudio::CAAudioFormat::CAAudioFormat(const AudioStreamBasicDescription& fo
 {
 	if(streamDescription_.mChannelsPerFrame > 2)
 		throw std::invalid_argument("More than two channels requires a channel layout");
+
+	if(streamDescription_.mChannelsPerFrame == 1)
+		channelLayout_ = CAChannelLayout::Mono;
+	else if(streamDescription_.mChannelsPerFrame == 2)
+		channelLayout_ = CAChannelLayout::Stereo;
 }
 
 CXXCoreAudio::CAAudioFormat::CAAudioFormat(const AudioStreamBasicDescription& format, const AudioChannelLayout *layout)
@@ -20,6 +25,14 @@ CXXCoreAudio::CAAudioFormat::CAAudioFormat(const AudioStreamBasicDescription& fo
 {
 	if(streamDescription_.mChannelsPerFrame > 2 && !channelLayout_)
 		throw std::invalid_argument("More than two channels requires a channel layout");
+
+	if(!channelLayout_) {
+		if(streamDescription_.mChannelsPerFrame == 1)
+			channelLayout_ = CAChannelLayout::Mono;
+		else if(streamDescription_.mChannelsPerFrame == 2)
+			channelLayout_ = CAChannelLayout::Stereo;
+	}
+
 	if(streamDescription_.mChannelsPerFrame != channelLayout_.ChannelCount())
 		throw std::invalid_argument("Channel count mismatch between stream description and channel layout");
 }
