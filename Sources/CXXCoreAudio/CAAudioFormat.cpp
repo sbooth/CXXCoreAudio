013f16735f5d336636719fba8857ea1b2c3d18d0
Stephen F. Booth <me@sbooth.org>
2025-11-24 21:44:26 -0600

    Add `CAAudioFormat` class

---
+++ Sources/CXXCoreAudio/CAAudioFormat.cpp
@@ -0,0 +1,25 @@
+//
+// Copyright © 2025 Stephen F. Booth
+// Part of https://github.com/sbooth/CXXCoreAudio
+// MIT license
+//
+
+#import <stdexcept>
+
+#import "CAAudioFormat.hpp"
+
+CXXCoreAudio::CAAudioFormat::CAAudioFormat(const AudioStreamBasicDescription& format)
+: streamDescription_{format}
+{
+	if(streamDescription_.mChannelsPerFrame > 2)
+		throw std::invalid_argument("More than two channels requires a channel layout");
+}
+
+CXXCoreAudio::CAAudioFormat::CAAudioFormat(const AudioStreamBasicDescription& format, const AudioChannelLayout *layout)
+: streamDescription_{format}, channelLayout_{layout}
+{
+	if(streamDescription_.mChannelsPerFrame > 2 && !channelLayout_)
+		throw std::invalid_argument("More than two channels requires a channel layout");
+	if(streamDescription_.mChannelsPerFrame != channelLayout_.ChannelCount())
+		throw std::invalid_argument("Channel count mismatch between stream description and channel layout");
+}
