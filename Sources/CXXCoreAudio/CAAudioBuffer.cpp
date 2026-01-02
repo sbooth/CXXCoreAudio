f4cd3990a2b3a584890b3bd51ef9c602e5c58a03
Stephen Booth <me@sbooth.org>
2026-01-02 17:52:15 -0600

    Add SPDX tags

--- Sources/CXXCoreAudio/CAAudioBuffer.cpp
+++ Sources/CXXCoreAudio/CAAudioBuffer.cpp
@@ -1,7 +1,8 @@
 //
-// Copyright © 2013-2025 Stephen F. Booth
+// SPDX-FileCopyrightText: 2013 Stephen F. Booth <contact@sbooth.dev>
+// SPDX-License-Identifier: MIT
+//
 // Part of https://github.com/sbooth/CXXCoreAudio
-// MIT license
 //
 
 #import <algorithm>
