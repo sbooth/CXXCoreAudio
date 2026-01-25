2a519e007de1ac01c6f4710cc851c2ccb72425d6
Stephen Booth <me@sbooth.org>
2026-01-25 13:28:32 -0600

    Reformat

--- Sources/CXXCoreAudio/CAAudioFormat.cpp
+++ Sources/CXXCoreAudio/CAAudioFormat.cpp
@@ -4,35 +4,38 @@
 // MIT license
 //
 
-#import <stdexcept>
-
 #import "CAAudioFormat.hpp"
 
-CXXCoreAudio::CAAudioFormat::CAAudioFormat(const AudioStreamBasicDescription& format)
-: streamDescription_{format}
-{
-	if(streamDescription_.mChannelsPerFrame > 2)
-		throw std::invalid_argument("More than two channels requires a channel layout");
+#import <stdexcept>
 
-	if(streamDescription_.mChannelsPerFrame == 1)
-		channelLayout_ = CAChannelLayout::Mono;
-	else if(streamDescription_.mChannelsPerFrame == 2)
-		channelLayout_ = CAChannelLayout::Stereo;
+CXXCoreAudio::CAAudioFormat::CAAudioFormat(const AudioStreamBasicDescription& format)
+  : streamDescription_{format} {
+    if (streamDescription_.mChannelsPerFrame > 2) {
+        throw std::invalid_argument("More than two channels requires a channel layout");
+    }
+
+    if (streamDescription_.mChannelsPerFrame == 1) {
+        channelLayout_ = CAChannelLayout::Mono;
+    } else if (streamDescription_.mChannelsPerFrame == 2) {
+        channelLayout_ = CAChannelLayout::Stereo;
+    }
 }
 
 CXXCoreAudio::CAAudioFormat::CAAudioFormat(const AudioStreamBasicDescription& format, const AudioChannelLayout *layout)
-: streamDescription_{format}, channelLayout_{layout}
-{
-	if(streamDescription_.mChannelsPerFrame > 2 && !channelLayout_)
-		throw std::invalid_argument("More than two channels requires a channel layout");
+  : streamDescription_{format}, channelLayout_{layout} {
+    if (streamDescription_.mChannelsPerFrame > 2 && !channelLayout_) {
+        throw std::invalid_argument("More than two channels requires a channel layout");
+    }
 
-	if(!channelLayout_) {
-		if(streamDescription_.mChannelsPerFrame == 1)
-			channelLayout_ = CAChannelLayout::Mono;
-		else if(streamDescription_.mChannelsPerFrame == 2)
-			channelLayout_ = CAChannelLayout::Stereo;
-	}
+    if (!channelLayout_) {
+        if (streamDescription_.mChannelsPerFrame == 1) {
+            channelLayout_ = CAChannelLayout::Mono;
+        } else if (streamDescription_.mChannelsPerFrame == 2) {
+            channelLayout_ = CAChannelLayout::Stereo;
+        }
+    }
 
-	if(streamDescription_.mChannelsPerFrame != channelLayout_.ChannelCount())
-		throw std::invalid_argument("Channel count mismatch between stream description and channel layout");
+    if (streamDescription_.mChannelsPerFrame != channelLayout_.ChannelCount()) {
+        throw std::invalid_argument("Channel count mismatch between stream description and channel layout");
+    }
 }
--- Sources/CXXCoreAudio/include/CXXCoreAudio/CAAudioFormat.hpp
+++ Sources/CXXCoreAudio/include/CXXCoreAudio/CAAudioFormat.hpp
@@ -17,63 +17,58 @@ namespace CXXCoreAudio {
 
 /// A class representing an audio format with both a stream description and channel layout.
 class CAAudioFormat final {
-public:
-	CAAudioFormat() noexcept = delete;
-	CAAudioFormat(const CAAudioFormat& other) = default;
-	CAAudioFormat(CAAudioFormat&& other) = default;
-	CAAudioFormat& operator=(const CAAudioFormat& other) = default;
-	CAAudioFormat& operator=(CAAudioFormat&& other) = default;
-	~CAAudioFormat() noexcept = default;
+  public:
+    CAAudioFormat() noexcept = delete;
+    CAAudioFormat(const CAAudioFormat& other) = default;
+    CAAudioFormat(CAAudioFormat&& other) = default;
+    CAAudioFormat& operator=(const CAAudioFormat& other) = default;
+    CAAudioFormat& operator=(CAAudioFormat&& other) = default;
+    ~CAAudioFormat() noexcept = default;
 
-	/// Creates an audio format with the specified stream description and no channel layout.
-	///
-	/// It is an error to specify a stream description with more than two channels.
-	/// @throw std::invalid_argument
-	explicit CAAudioFormat(const AudioStreamBasicDescription& format);
+    /// Creates an audio format with the specified stream description and no channel layout.
+    ///
+    /// It is an error to specify a stream description with more than two channels.
+    /// @throw std::invalid_argument
+    explicit CAAudioFormat(const AudioStreamBasicDescription& format);
 
-	/// Creates an audio format with the specified stream description and channel layout.
-	///
-	/// It is an error to specify a stream description with more than two channels with a null channel layout,
-	/// or a stream description and channel layout with unequal channel counts.
-	/// @throw std::invalid_argument
-	CAAudioFormat(const AudioStreamBasicDescription& format, const AudioChannelLayout *layout);
+    /// Creates an audio format with the specified stream description and channel layout.
+    ///
+    /// It is an error to specify a stream description with more than two channels with a null channel layout,
+    /// or a stream description and channel layout with unequal channel counts.
+    /// @throw std::invalid_argument
+    CAAudioFormat(const AudioStreamBasicDescription& format, const AudioChannelLayout *layout);
 
-	/// Returns true if this audio format is equal to another.
-	bool operator==(const CAAudioFormat& other) const noexcept
-	{
-		return streamDescription_ == other.streamDescription_ && channelLayout_ == other.channelLayout_;
-	}
+    /// Returns true if this audio format is equal to another.
+    bool operator==(const CAAudioFormat& other) const noexcept {
+        return streamDescription_ == other.streamDescription_ && channelLayout_ == other.channelLayout_;
+    }
 
-	/// Returns true if this audio format is not equal to another.
-	bool operator!=(const CAAudioFormat& other) const noexcept
-	{
-		return streamDescription_ != other.streamDescription_ || channelLayout_ != other.channelLayout_;
-	}
+    /// Returns true if this audio format is not equal to another.
+    bool operator!=(const CAAudioFormat& other) const noexcept {
+        return streamDescription_ != other.streamDescription_ || channelLayout_ != other.channelLayout_;
+    }
 
-	/// Returns the format's stream description.
-	const CAStreamDescription& StreamDescription() const noexcept
-	{
-		return streamDescription_;
-	}
+    /// Returns the format's stream description.
+    const CAStreamDescription& StreamDescription() const noexcept {
+        return streamDescription_;
+    }
 
-	/// Returns the format's channel layout.
-	const CAChannelLayout& ChannelLayout() const noexcept
-	{
-		return channelLayout_;
-	}
+    /// Returns the format's channel layout.
+    const CAChannelLayout& ChannelLayout() const noexcept {
+        return channelLayout_;
+    }
 
 #ifdef __OBJC__
-	operator AVAudioFormat * _Nullable () const noexcept
-	{
-		return [[AVAudioFormat alloc] initWithStreamDescription:&streamDescription_ channelLayout:channelLayout_];
-	}
+    operator AVAudioFormat *_Nullable() const noexcept {
+        return [[AVAudioFormat alloc] initWithStreamDescription:&streamDescription_ channelLayout:channelLayout_];
+    }
 #endif /* __OBJC__ */
 
-private:
-	/// The format's stream description.
-	CAStreamDescription streamDescription_{};
-	/// The format's channel layout.
-	CAChannelLayout channelLayout_{};
+  private:
+    /// The format's stream description.
+    CAStreamDescription streamDescription_{};
+    /// The format's channel layout.
+    CAChannelLayout channelLayout_{};
 };
 
 } /* namespace CXXCoreAudio */
